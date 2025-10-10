29f63d93708b69d0e5f3d3b78a5da8fcc34e9142
Rahul <rahulkilaparthi7@gmail.com>
2025-10-10 22:38:55 +0530

    Configure for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/Attendance-Calculator-Pro/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
